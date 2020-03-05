a007fc6cb6df7af15147d212a02c102f9b2cf01c
Hayley Ha <hami201289@him-mail.ch>
2020-03-06 09:32:44 +1300

    api responds to requests

--- api/Startup.cs
+++ api/Startup.cs
@@ -36,7 +36,7 @@ namespace api
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
