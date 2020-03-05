36f20d26a1d94747fad09a5b3655fc85cd8e526b
Hayley Ha <hami201289@him-mail.ch>
2020-03-06 10:52:24 +1300

    configure for DataContext and added migration

--- api/Startup.cs
+++ api/Startup.cs
@@ -6,12 +6,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistence;
 
-namespace api
+namespace Api
 {
     public class Startup
     {
@@ -25,6 +27,9 @@ namespace api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(opt => {
+                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            });
             services.AddControllers();
         }
 
--- persistence/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace persistence
-{
-    public class Class1
-    {
-    }
-}
