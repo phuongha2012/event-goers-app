3c2059f1f82a6fb601edbb65b297373ed8ab129a
phuongha2012 <hami201289@him-mail.ch>
2020-07-30 15:35:55 +1200

    add react client

--- api/Startup.cs
+++ api/Startup.cs
@@ -30,6 +30,13 @@ namespace Api
             services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+
             services.AddControllers();
         }
 
@@ -43,14 +50,9 @@ namespace Api
 
             // app.UseHttpsRedirection();
 
-            app.UseRouting();
+            app.UseCors("CorsPolicy");
 
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseMvc();
         }
     }
 }
