684c1ea7ca10b4017ebd3cd2a88b0c2efe9e221f
phuongha2012 <hami201289@him-mail.ch>
2020-08-02 07:40:18 +1200

    seed data

--- api/Program.cs
+++ api/Program.cs
@@ -25,6 +25,7 @@ namespace Api
                 {
                     var context = services.GetRequiredService<DataContext>();
                     context.Database.Migrate();
+                    Seed.SeedData(context);
                 }
                 catch (Exception ex)
                 {
