c1dc7fbb82711fbae27a55aae6bf0fbc2bfe6d5c
phuongha2012 <hami201289@him-mail.ch>
2020-08-04 14:46:26 +1200

    added ActivityList and ActivityDetails components

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { Header, Icon, List, Container } from 'semantic-ui-react';
 import { IActivity } from '../models/activity';
 import { NavBar } from '../../features/nav/NavBar';
+import { ActivityDashboard } from '../../features/activities/dashboard/ActivityDashboard';
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
@@ -19,11 +20,7 @@ const App = () => {
     <Fragment>
       <NavBar />
       <Container style={{marginTop: '7em'}}>
-        <List>
-          {activities.map(activity => (
-            <List.Item key={activity.id}>{activity.title}</List.Item>
-          ))}
-        </List>
+        <ActivityDashboard activities={activities}/>
       </Container>
     </Fragment>
   );
---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Grid, List } from 'semantic-ui-react'
+import { IActivity } from '../../../app/models/activity'
+import { ActivityList } from './ActivityList'
+import { ActivityDetails } from '../details/ActivityDetails'
+
+interface IProps {
+    activities: IActivity[]
+}
+
+export const ActivityDashboard: React.FC<IProps> = ({activities}) => {
+    return (
+        <Grid>
+            <Grid.Column width={10}>
+                <ActivityList activities={activities} />
+            </Grid.Column>
+            <Grid.Column width={6}>
+                <ActivityDetails />
+            </Grid.Column>
+        </Grid>
+    )
+}
+
