3620178e9c482a811731859063c250ae3d8a7d80
phuongha2012 <hami201289@him-mail.ch>
2020-08-05 15:37:17 +1200

    all CRUD actions persisted in db

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -8,14 +8,14 @@ const responseBody = (response: AxiosResponse) => response.data;
 const requests = {
     get: (url: string) => axios.get(url).then(responseBody),
     post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
-    put: (url: string, body: {}) => axios.post(url, body).then(responseBody),
+    put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
     del: (url: string)  => axios.delete(url).then(responseBody)
 }
 
 const Activities = {
-    list: () => requests.get('/activities'),
+    list: (): Promise<IActivity[]> => requests.get('/activities'),
     details: (id: string) => requests.get(`/activities/${id}`),
-    create: (activity: IActivity) => requests.post('/activities', activity),
+    create: (activity: IActivity) => requests.post('/activities/', activity),
     update: (activity: IActivity) => requests.put(`/activities/${activity.id}`, activity),
     delete: (id: string) => requests.del(`/activities/${id}`)
 }
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,17 +1,19 @@
-import React, { useState, useEffect, Fragment } from 'react';
-import axios from 'axios';
-import { Container } from 'semantic-ui-react';
-import { IActivity } from '../models/activity';
-import { NavBar } from '../../features/nav/NavBar';
-import { ActivityDashboard } from '../../features/activities/dashboard/ActivityDashboard';
+import React, { useState, useEffect, Fragment } from "react";
+import { Container } from "semantic-ui-react";
+import { IActivity } from "../models/activity";
+import { NavBar } from "../../features/nav/NavBar";
+import { ActivityDashboard } from "../../features/activities/dashboard/ActivityDashboard";
+import agent from "../api/agent";
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
+  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
+    null
+  );
   const [editMode, setEditMode] = useState(false);
 
   const handleSelectActivity = (id: string) => {
-    setSelectedActivity(activities.filter(a => a.id === id)[0]);
+    setSelectedActivity(activities.filter((a) => a.id === id)[0]);
     setEditMode(false);
   };
 
@@ -21,40 +23,47 @@ const App = () => {
   };
 
   const handleCreateActivity = (activity: IActivity) => {
-    setActivities([...activities, activity]);
-    setSelectedActivity(activity);
-    setEditMode(false);
+    agent.Activities.create(activity).then(() => {
+      setActivities([...activities, activity]);
+      setSelectedActivity(activity);
+      setEditMode(false);
+    });
   };
 
   const handleEditActivity = (activity: IActivity) => {
-    setActivities([...activities.filter(a => a.id !== activity.id), activity]);
-    setSelectedActivity(activity);
-    setEditMode(false);
+    agent.Activities.update(activity).then(() => {
+      setActivities([
+        ...activities.filter((a) => a.id !== activity.id),
+        activity,
+      ]);
+      setSelectedActivity(activity);
+      setEditMode(false);
+    });
   };
 
   const handleDeleteActivity = (id: string) => {
-    setActivities([...activities.filter(activity => activity.id !== id)])
+    agent.Activities.delete(id).then(() => {
+      setActivities([...activities.filter((activity) => activity.id !== id)]);
+    });
   };
 
   useEffect(() => {
-    axios
-      .get<IActivity[]>("http://localhost:5000/api/activities")
-      .then(response => {
-        let activities : IActivity[] = [];
-        response.data.forEach(activity => {
-          activity.date = activity.date.split('.')[0];
-          activities.push(activity);
-        })
-        setActivities(activities);
+    agent.Activities.list().then((response) => {
+      let activities: IActivity[] = [];
+      response.forEach((activity) => {
+        activity.date = activity.date.split(".")[0];
+        activities.push(activity);
       });
+      setActivities(activities);
+    });
   }, []);
 
   return (
     <Fragment>
       <NavBar openCreateForm={handleOpenCreateForm} />
-      <Container style={{marginTop: '7em'}}>
-        <ActivityDashboard 
-          activities={activities} 
+      <Container style={{ marginTop: "7em" }}>
+        <ActivityDashboard
+          activities={activities}
           selectActivity={handleSelectActivity}
           selectedActivity={selectedActivity!}
           setSelectedActivity={setSelectedActivity}
@@ -67,6 +76,6 @@ const App = () => {
       </Container>
     </Fragment>
   );
-}
+};
 
 export default App;
