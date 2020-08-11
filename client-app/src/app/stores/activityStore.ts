214b5e811030ab6cd0d6557725b120cec6603197
phuongha2012 <hami201289@him-mail.ch>
2020-08-11 15:27:05 +1200

    separate activities into date group

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -13,8 +13,20 @@ class ActivityStore {
     @observable target = '';
 
     @computed get activitiesByDate() {
-        return Array.from(this.activityRegistry.values()).sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+        // return Array.from(this.activityRegistry.values()).sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+        return this.groupActivitiesByDate(Array.from(this.activityRegistry.values()))
     }
+
+    groupActivitiesByDate(activities: IActivity[]) {
+        const sortedActivites = activities.sort(
+            (a, b) => Date.parse(a.date) - Date.parse(b.date)
+        )
+        return Object.entries(sortedActivites.reduce((activities, activity) => {
+            const date = activity.date.split('T')[0];
+            activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+            return activities;
+        }, {} as {[key: string]: IActivity[]}));
+    } 
 
     @action loadActivities = async () => {
         this.loadingInitial = true;
