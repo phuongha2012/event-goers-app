3a64493679d6f90efbde7afde3cdd087b251c736
phuongha2012 <hami201289@him-mail.ch>
2020-08-11 15:41:28 +1200

    adding more style to ActivityList

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,39 +1,39 @@
-import React, { useContext } from "react";
-import { Item, Button, Label } from "semantic-ui-react";
+import React from "react";
+import { Item, Button, Label, Segment, Icon } from "semantic-ui-react";
 import { Link } from "react-router-dom";
-import ActivityStore from "../../../app/stores/activityStore";
 import { IActivity } from "../../../app/models/activity";
 
-export const ActivityListItem: React.FC<{activity: IActivity}> = ({activity}) => {
-  const activityStore = useContext(ActivityStore);
-  const {
-    activitiesByDate,
-    deleteActivity,
-    submitting,
-    target,
-  } = activityStore;
+export const ActivityListItem: React.FC<{ activity: IActivity }> = ({
+  activity,
+}) => {
   return (
-    <Item key={activity.id}>
-      <Item.Content>
-        <Item.Header as="a">{activity.title}</Item.Header>
-        <Item.Meta>{activity.date}</Item.Meta>
-        <Item.Description>
-          <div>{activity.description}</div>
-          <div>
-            {activity.city}, {activity.venue}
-          </div>
-        </Item.Description>
-        <Item.Extra>
-          <Button
-            as={Link}
-            to={`/activities/${activity.id}`}
-            floated="right"
-            content="View"
-            color="blue"
-          />
-          <Label basic content={activity.category} />
-        </Item.Extra>
-      </Item.Content>
-    </Item>
+    <Segment.Group>
+      <Segment>
+        <Item.Group>
+          <Item key={activity.id}>
+            <Item.Image size="tiny" circular src="/assets/user.png" />
+            <Item.Content>
+              <Item.Header as="a">{activity.title}</Item.Header>
+              <Item.Description>Hosted by</Item.Description>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </Segment>
+      <Segment>
+        <Icon name="clock" /> {activity.date}
+        <Icon name="marker" /> {activity.venue}, {activity.city}
+      </Segment>
+      <Segment secondary>Attendees go here</Segment>
+      <Segment clearing>
+        <span>{activity.description}</span>
+        <Button
+          as={Link}
+          to={`/activities/${activity.id}`}
+          floated="right"
+          content="View"
+          color="blue"
+        />
+      </Segment>
+    </Segment.Group>
   );
 };
