d27652d2c95e23091fae8435f74caa6acf8bef4a
phuongha2012 <hami201289@him-mail.ch>
2020-08-14 14:24:17 +1200

    use date-fns format to render Date

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Item, Button, Segment, Icon } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import { IActivity } from "../../../app/models/activity";
+import { format } from 'date-fns';
 
 export const ActivityListItem: React.FC<{ activity: IActivity }> = ({
   activity,
@@ -20,7 +21,7 @@ export const ActivityListItem: React.FC<{ activity: IActivity }> = ({
         </Item.Group>
       </Segment>
       <Segment>
-        <Icon name="clock" /> {activity.date}
+        <Icon name="clock" /> {format(activity.date!, 'h:mm a')}
         <Icon name="marker" /> {activity.venue}, {activity.city}
       </Segment>
       <Segment secondary>Attendees go here</Segment>
