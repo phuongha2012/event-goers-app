c2c9d6303dec05a65d8fffa1648f30c12f3e79e3
phuongha2012 <hami201289@him-mail.ch>
2020-08-16 17:29:08 +1200

    create new user object with less details for login request

---
+++ api/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+using System.Threading.Tasks;
+using Application.User;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    public class UserController : BaseController
+    {
+        [HttpPost("login")]
+        public async Task<ActionResult<User>> Login(Login.Query query) 
+        {
+            return await Mediator.Send(query);
+        }
+    }
+}
--- application/User/Login.cs
+++ application/User/Login.cs
@@ -12,7 +12,7 @@ namespace Application.User
 {
     public class Login
     {
-        public class Query : IRequest<AppUser>
+        public class Query : IRequest<User>
         {
             public string Email { get; set; }
             public string Password { get; set; }
@@ -27,7 +27,7 @@ namespace Application.User
             }
         }
 
-        public class Handler : IRequestHandler<Query, AppUser>
+        public class Handler : IRequestHandler<Query, User>
         {
             private readonly UserManager<AppUser> _userManager;
             private readonly SignInManager<AppUser> _signInManager;
@@ -37,7 +37,7 @@ namespace Application.User
                 _userManager = userManager;
             }
 
-            public async Task<AppUser> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<User> Handle(Query request, CancellationToken cancellationToken)
             {
                 var user = await _userManager.FindByEmailAsync(request.Email);
 
@@ -48,7 +48,13 @@ namespace Application.User
 
                 if (result.Succeeded)
                 {
-                    return user;
+                    return new User
+                    {
+                        DisplayName = user.DisplayName,
+                        Token = "token",
+                        Username = user.UserName,
+                        Image = null
+                    };
                 }
                 throw new RestException(HttpStatusCode.Unauthorized);
             }
