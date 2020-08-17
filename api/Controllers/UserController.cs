94a3ae2a3a3fd7f85630ddc6bf16d524d09fc5f2
phuongha2012 <hami201289@him-mail.ch>
2020-08-17 14:34:13 +1200

    configure policy to require authentication to access all routes

--- api/Controllers/UserController.cs
+++ api/Controllers/UserController.cs
@@ -1,10 +1,12 @@
 using System.Threading.Tasks;
 using Application.User;
 using Domain;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
 {
+    [AllowAnonymous]
     public class UserController : BaseController
     {
         [HttpPost("login")]
--- api/Startup.cs
+++ api/Startup.cs
@@ -11,11 +11,13 @@ using FluentValidation.AspNetCore;
 using Infrastructure.Security;
 using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -53,8 +55,12 @@ namespace Api
 
             services.AddMediatR(typeof(List.Handler).Assembly);
 
-            services.AddControllers()
-                .AddFluentValidation(cfg => 
+            services.AddControllers(opt =>
+            {
+                var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+                opt.Filters.Add(new AuthorizeFilter(policy));
+            })
+                .AddFluentValidation(cfg =>
                 {
                     cfg.RegisterValidatorsFromAssemblyContaining<Create>();
                 });
@@ -67,7 +73,7 @@ namespace Api
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"]));
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(opt => 
+                .AddJwtBearer(opt =>
                 {
                     opt.TokenValidationParameters = new TokenValidationParameters
                     {
