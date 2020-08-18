524da80da509eff3a17675fc4d6addd59bd68650
phuongha2012 <hami201289@him-mail.ch>
2020-08-18 15:47:44 +1200

    got back loggedin user object from backend

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -16,7 +16,7 @@ const HomePage = () => {
           Reactivities
         </Header>
         <Header as="h2" inverted content="Welcome to Reactivities" />
-        <Button as={Link} to="/activities" size="huge" inverted>
+        <Button as={Link} to="/login" size="huge" inverted>
           Take me to the activities!
         </Button>
       </Container>
