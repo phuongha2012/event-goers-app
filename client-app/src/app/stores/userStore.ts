e587e7438d45f5140a7a98d265ca57bf2a8f1609
phuongha2012 <hami201289@him-mail.ch>
2020-08-19 06:54:43 +1200

    set and display server error in login form's FORM_ERROR

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -2,6 +2,7 @@ import { observable, computed, action, runInAction } from "mobx";
 import { IUser, IUserFormValues } from "../models/user";
 import agent from "../api/agent";
 import { RootStore } from "./rootStore";
+import { history } from "../..";
 
 export default class UserStore {
     rootStore: RootStore;
@@ -19,8 +20,9 @@ export default class UserStore {
                 this.user = user;
             })
             console.log(user);
+            history.push('/activities');
         } catch (error) {
-            console.log(error);
+            throw error;
         }
     }
 }
--- client-app/src/features/user/LoginForm.tsx
+++ client-app/src/features/user/LoginForm.tsx
@@ -1,18 +1,38 @@
 import React, { useContext } from "react";
 import { Form as FinalForm, Field } from "react-final-form";
-import { Form, Button } from "semantic-ui-react";
+import { Form, Button, Label } from "semantic-ui-react";
 import TextInput from "../../app/common/form/TextInput";
 import { RootStoreContext } from "../../app/stores/rootStore";
 import { IUserFormValues } from "../../app/models/user";
+import { FORM_ERROR } from "final-form";
+import { combineValidators, isRequired } from "revalidate";
+
+const validate = combineValidators({
+  email: isRequired("email"),
+  password: isRequired("password"),
+});
 
 const LoginForm = () => {
-    const rootStore = useContext(RootStoreContext);
-    const {login} = rootStore.userStore;
-    
+  const rootStore = useContext(RootStoreContext);
+  const { login } = rootStore.userStore;
+
   return (
     <FinalForm
-      onSubmit={(values: IUserFormValues) => login(values)}
-      render={({ handleSubmit }) => (
+      onSubmit={(values: IUserFormValues) =>
+        login(values).catch((error) => ({
+          [FORM_ERROR]: error,
+        }))
+      }
+      validate={validate}
+      render={({
+        handleSubmit,
+        submitting,
+        form,
+        submitError,
+        invalid,
+        pristine,
+        dirtySinceLastSubmit
+      }) => (
         <Form onSubmit={handleSubmit}>
           <Field name="email" component={TextInput} placeholder="Email" />
           <Field
@@ -21,7 +41,16 @@ const LoginForm = () => {
             placeholder="Password"
             type="password"
           />
-          <Button positive content='Login' />
+          {submitError && !dirtySinceLastSubmit && (
+            <Label color="red" basic content={submitError.statusText} />
+          )}
+          <Button
+            disabled={invalid && !dirtySinceLastSubmit || pristine}
+            loading={submitting}
+            positive
+            content="Login"
+          />
+          <pre>{JSON.stringify(form.getState(), null, 2)}</pre>
         </Form>
       )}
     />
