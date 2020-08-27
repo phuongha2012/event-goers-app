c4ac6c475c1bcd2e0a0ce5d6c24b6a00eed760dc
phuongha2012 <hami201289@him-mail.ch>
2020-08-27 15:27:15 +1200

    add unattending functionality

--- api/Controllers/ActivitiesController.cs
+++ api/Controllers/ActivitiesController.cs
@@ -50,5 +50,11 @@ namespace Api.Controllers
         {
             return await Mediator.Send(new Attend.Command{Id = id});
         }
+
+        [HttpDelete("{id}/attend")]
+        public async Task<ActionResult<Unit>> Unattend(Guid id)
+        {
+            return await Mediator.Send(new Unattend.Command{Id = id});
+        }
     }
 }
