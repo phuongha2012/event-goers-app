17bed41401611d4717e19500862d40b0fcd6fe56
phuongha2012 <hami201289@him-mail.ch>
2020-09-01 12:21:07 +1200

    add uploadPhoto functionality to frontend

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -2,6 +2,7 @@ import { RootStore } from "./rootStore";
 import { runInAction, observable, action, computed } from "mobx";
 import { IProfile } from "../models/profile";
 import agent from "../api/agent";
+import { toast } from "react-toastify";
 
 export default class ProfileStore {
     rootStore: RootStore
@@ -12,6 +13,7 @@ export default class ProfileStore {
 
     @observable profile: IProfile | null = null;
     @observable loadingProfile = true;
+    @observable uploadingPhoto = false;
 
     @computed get isCurrentUser() {
         if (this.rootStore.userStore.user && this.profile) {
@@ -34,6 +36,27 @@ export default class ProfileStore {
                 this.loadingProfile = false;
             })
             console.log(error);
+        }
+    }
+
+    @action uploadPhoto = async (file: Blob) => {
+        this.uploadingPhoto = true;
+        try {
+            const photo = await agent.Profiles.uploadPhoto(file);
+            runInAction(() => {
+                this.profile!.photos.push(photo);
+                if (photo.isMain && this.rootStore.userStore.user) {
+                    this.rootStore.userStore.user.image = photo.url;
+                    this.profile!.image = photo.url
+                }
+                this.uploadingPhoto = false;
+            })
+        } catch (error) {
+            console.log(error);
+            toast.error('Problem uploading photo');
+            runInAction(() => {
+                this.uploadingPhoto = false;
+            })
         }
     }
 }
