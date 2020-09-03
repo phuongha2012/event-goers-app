84ae42e3898cfae0e1ba4684ff00efe51240ab5e
phuongha2012 <hami201289@him-mail.ch>
2020-09-04 11:26:47 +1200

    finished ActivityProfile component

--- client-app/src/features/profiles/ActivityProfile.tsx
+++ client-app/src/features/profiles/ActivityProfile.tsx
@@ -1,20 +1,47 @@
-import React, { useEffect, useContext } from 'react'
-import { observer } from 'mobx-react-lite';
-import { RootStoreContext } from '../../app/stores/rootStore';
-import { toJS } from 'mobx';
+import React, { useContext } from "react";
+import { observer } from "mobx-react-lite";
+import { RootStoreContext } from "../../app/stores/rootStore";
+import { toJS } from "mobx";
+import { Header, Tab, Card } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 const ActivityProfile = () => {
-    const rootStore = useContext(RootStoreContext);
-    const activitiesToGo = rootStore.activityStore.activititesGoingByUser;
-    const activitiesToHost = rootStore.activityStore.activititesHostingByUser;
-    useEffect(() => {
-        console.log(activitiesToHost);
-    })
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const rootStore = useContext(RootStoreContext);
+  const activitiesToGo = rootStore.activityStore.activititesGoingByUser;
+  const activitiesToHost = rootStore.activityStore.activititesHostingByUser;
+  console.log(activitiesToGo);
+  return (
+    <Tab.Pane>
+      <Header size="medium">Going:</Header>
+      {activitiesToGo.map(activity => (
+          <Card
+          fluid
+          color='pink'
+          key={activity.id}
+          as={Link}
+          to={`/activities/${activity.id}`}
+          header={activity.title}
+          meta={activity.date.toDateString()}
+          description={activity.description}
+        />
+      ))}
+
+      
+      <Header size="medium">Hosting:</Header>
+      {activitiesToHost.map(activity => (
+          <Card
+          fluid
+          color='orange'
+          key={activity.id}
+          as={Link}
+          to={`/activities/${activity.id}`}
+          header={activity.title}
+          meta={activity.date.toDateString()}
+          description={activity.description}
+        />
+      ))}
+    </Tab.Pane>
+  );
+};
 
 export default observer(ActivityProfile);
