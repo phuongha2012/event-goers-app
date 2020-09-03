37399949185eeb31758ac527e7b2de9f1b0df37b
phuongha2012 <hami201289@him-mail.ch>
2020-09-03 16:07:00 +1200

    fix bug Value cannot be null. (Parameter 'source')

--- application/User/Register.cs
+++ application/User/Register.cs
@@ -72,8 +72,7 @@ namespace Application.User
                     {
                         DisplayName = user.DisplayName,
                         Token = _jwtGenerator.CreateToken(user),
-                        Username = user.UserName,
-                        Image = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
+                        Username = user.UserName
                     };
                 }
 
