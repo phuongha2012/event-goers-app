2f56fe78b87999e33c9cc6bc0245b27c6437b19f
phuongha2012 <hami201289@him-mail.ch>
2020-09-05 07:47:38 +1200

    modified seed data

--- persistence/Seed.cs
+++ persistence/Seed.cs
@@ -19,23 +19,51 @@ namespace Persistence
                     new AppUser
                     {
                         Id = "a",
-                        DisplayName = "Bob",
-                        UserName = "bob",
-                        Email = "bob@test.com"
+                        DisplayName = "Brian",
+                        UserName = "brian",
+                        Email = "brian@test.com"
                     },
                     new AppUser
                     {
                         Id = "b",
-                        DisplayName = "Jane",
-                        UserName = "jane",
-                        Email = "jane@test.com"
+                        DisplayName = "Tracey",
+                        UserName = "tracey",
+                        Email = "tracey@test.com"
                     },
                     new AppUser
                     {
                         Id = "c",
-                        DisplayName = "Tom",
-                        UserName = "tom",
-                        Email = "tom@test.com"
+                        DisplayName = "Taylor",
+                        UserName = "taylor",
+                        Email = "taylor@test.com"
+                    },
+                    new AppUser
+                    {
+                        Id = "d",
+                        DisplayName = "Patrick",
+                        UserName = "patrick",
+                        Email = "patrick@test.com"
+                    },
+                    new AppUser
+                    {
+                        Id = "e",
+                        DisplayName = "Gabrielle",
+                        UserName = "gabrielle",
+                        Email = "gabrielle@test.com"
+                    },
+                    new AppUser
+                    {
+                        Id = "f",
+                        DisplayName = "Jack",
+                        UserName = "jack",
+                        Email = "jack@test.com"
+                    },
+                    new AppUser
+                    {
+                        Id = "g",
+                        DisplayName = "Marina",
+                        UserName = "maria",
+                        Email = "maria@test.com"
                     },
                 };
 
@@ -51,54 +79,12 @@ namespace Persistence
                 {
                     new Activity
                     {
-                        Title = "Past Activity 1",
-                        Date = DateTime.Now.AddMonths(-2),
-                        Description = "Activity 2 months ago",
-                        Category = "Drinks",
-                        City = "London",
-                        Venue = "Pub",
-                        UserActivities = new List<UserActivity>
-                        {
-                            new UserActivity
-                            {
-                                AppUserId = "a",
-                                IsHost = true,
-                                DateJoined = DateTime.Now.AddMonths(-2)
-                            }
-                        }
-                    },
-                    new Activity
-                    {
-                        Title = "Past Activity 2",
-                        Date = DateTime.Now.AddMonths(-1),
-                        Description = "Activity 1 month ago",
-                        Category = "Culture",
-                        City = "Paris",
-                        Venue = "The Louvre",
-                        UserActivities = new List<UserActivity>
-                        {
-                            new UserActivity
-                            {
-                                AppUserId = "b",
-                                IsHost = true,
-                                DateJoined = DateTime.Now.AddMonths(-1)
-                            },
-                            new UserActivity
-                            {
-                                AppUserId = "a",
-                                IsHost = false,
-                                DateJoined = DateTime.Now.AddMonths(-1)
-                            },
-                        }
-                    },
-                    new Activity
-                    {
-                        Title = "Future Activity 1",
+                        Title = "Tribal Drum & Dance Jam",
                         Date = DateTime.Now.AddMonths(1),
-                        Description = "Activity 1 month in future",
+                        Description = "Dancers are welcome to come along to enjoy dancing to live music and learn more about Middle Eastern rhythms - switch between drumming & dancing as suits you!",
                         Category = "Music",
-                        City = "London",
-                        Venue = "Wembly Stadium",
+                        City = "Christchurch",
+                        Venue = "Showbiz Studios",
                         UserActivities = new List<UserActivity>
                         {
                             new UserActivity
@@ -113,15 +99,27 @@ namespace Persistence
                                 IsHost = false,
                                 DateJoined = DateTime.Now.AddMonths(1)
                             },
+                            new UserActivity
+                            {
+                                AppUserId = "f",
+                                IsHost = false,
+                                DateJoined = DateTime.Now.AddMonths(1)
+                            },
+                            new UserActivity
+                            {
+                                AppUserId = "e",
+                                IsHost = false,
+                                DateJoined = DateTime.Now.AddMonths(1)
+                            },
                         }
                     },
                     new Activity
                     {
-                        Title = "Future Activity 2",
+                        Title = "Lunch on the lawn",
                         Date = DateTime.Now.AddMonths(2),
-                        Description = "Activity 2 months in future",
+                        Description = "Come along to start the Term with a delicious free BBQ! Free snag or vege pattie as a welcome back to Term 4, see ya there!",
                         Category = "Food",
-                        City = "London",
+                        City = "Auckland",
                         Venue = "Jamies Italian",
                         UserActivities = new List<UserActivity>
                         {
@@ -136,28 +134,52 @@ namespace Persistence
                                 AppUserId = "a",
                                 IsHost = false,
                                 DateJoined = DateTime.Now.AddMonths(2)
+                            },new UserActivity
+                            {
+                                AppUserId = "b",
+                                IsHost = false,
+                                DateJoined = DateTime.Now.AddMonths(2)
                             },
+                            new UserActivity
+                            {
+                                AppUserId = "d",
+                                IsHost = false,
+                                DateJoined = DateTime.Now.AddMonths(2)
+                            },
+                            new UserActivity
+                            {
+                                AppUserId = "e",
+                                IsHost = false,
+                                DateJoined = DateTime.Now.AddMonths(2)
+                            },
+                            new UserActivity
+                            {
+                                AppUserId = "f",
+                                IsHost = false,
+                                DateJoined = DateTime.Now.AddMonths(2)
+                            },
+
                         }
                     },
                     new Activity
                     {
-                        Title = "Future Activity 3",
+                        Title = "Gin Tasting at Riccarton House with Wine Divine",
                         Date = DateTime.Now.AddMonths(3),
-                        Description = "Activity 3 months in future",
+                        Description = "The lovely Laura will take you through the history of some of the world’s best loved boutique gins along with some lesser known & unique bottles. Explore your taste buds by adding different garnishes & tonics to each gin. Paired nibbles will be served throughout the tasting.",
                         Category = "Drinks",
-                        City = "London",
-                        Venue = "Pub",
+                        City = "Canterbury",
+                        Venue = "Riccarton House",
                         UserActivities = new List<UserActivity>
                         {
                             new UserActivity
                             {
-                                AppUserId = "b",
+                                AppUserId = "f",
                                 IsHost = true,
                                 DateJoined = DateTime.Now.AddMonths(3)
                             },
                             new UserActivity
                             {
-                                AppUserId = "c",
+                                AppUserId = "a",
                                 IsHost = false,
                                 DateJoined = DateTime.Now.AddMonths(3)
                             },
@@ -165,12 +187,12 @@ namespace Persistence
                     },
                     new Activity
                     {
-                        Title = "Future Activity 4",
+                        Title = "Banff Mountain Film Festival World Tour – WELLINGTON 2020",
                         Date = DateTime.Now.AddMonths(4),
-                        Description = "Activity 4 months in future",
+                        Description = "Ignite your passion for adventure, action, and travel! The Banff Centre Mountain Film Festival World Tour will exhilarate you with amazing big-screen stories. Journey to exotic locations, paddle the wildest waters, and climb the highest peaks. Get your tickets today and be taken away to the most captivating places on earth.",
                         Category = "Culture",
-                        City = "London",
-                        Venue = "British Museum",
+                        City = "Wellington",
+                        Venue = "Embassy Theatre",
                         UserActivities = new List<UserActivity>
                         {
                             new UserActivity
@@ -178,17 +200,29 @@ namespace Persistence
                                 AppUserId = "a",
                                 IsHost = true,
                                 DateJoined = DateTime.Now.AddMonths(4)
+                            },
+                            new UserActivity
+                            {
+                                AppUserId = "b",
+                                IsHost = false,
+                                DateJoined = DateTime.Now.AddMonths(4)
+                            },
+                            new UserActivity
+                            {
+                                AppUserId = "c",
+                                IsHost = false,
+                                DateJoined = DateTime.Now.AddMonths(4)
                             }
                         }
                     },
                     new Activity
                     {
-                        Title = "Future Activity 5",
+                        Title = "High Tea at Le Cordon Bleu",
                         Date = DateTime.Now.AddMonths(5),
-                        Description = "Activity 5 months in future",
+                        Description = "Experience an exquisite selection of the finest French Pâtisserie prepared by our highly creative and talented Le Cordon Bleu NZ Students",
                         Category = "Drinks",
-                        City = "London",
-                        Venue = "Punch and Judy",
+                        City = "Wellington",
+                        Venue = "Le Cordon Bleu New Zealand",
                         UserActivities = new List<UserActivity>
                         {
                             new UserActivity
@@ -203,27 +237,39 @@ namespace Persistence
                                 IsHost = false,
                                 DateJoined = DateTime.Now.AddMonths(5)
                             },
+                            new UserActivity
+                            {
+                                AppUserId = "g",
+                                IsHost = false,
+                                DateJoined = DateTime.Now.AddMonths(5)
+                            },
+                            new UserActivity
+                            {
+                                AppUserId = "e",
+                                IsHost = false,
+                                DateJoined = DateTime.Now.AddMonths(5)
+                            },
                         }
                     },
                     new Activity
                     {
-                        Title = "Future Activity 6",
+                        Title = "Molly & the Chromatics live at Meow",
                         Date = DateTime.Now.AddMonths(6),
-                        Description = "Activity 6 months in future",
+                        Description = "Molly & The Chromatics hit the road this month for two stand alone shows! The band have recently confirmed the release of their debut Album with the first track Goddess now out on all platforms. With impending releases on the very near horizon be sure to grab a ticket early to see this brand new live set from M&TC.",
                         Category = "Music",
-                        City = "London",
-                        Venue = "O2 Arena",
+                        City = "Wellington",
+                        Venue = "Meow",
                         UserActivities = new List<UserActivity>
                         {
                             new UserActivity
                             {
-                                AppUserId = "a",
+                                AppUserId = "f",
                                 IsHost = true,
                                 DateJoined = DateTime.Now.AddMonths(6)
                             },
                             new UserActivity
                             {
-                                AppUserId = "b",
+                                AppUserId = "g",
                                 IsHost = false,
                                 DateJoined = DateTime.Now.AddMonths(6)
                             },
@@ -231,23 +277,23 @@ namespace Persistence
                     },
                     new Activity
                     {
-                        Title = "Future Activity 7",
+                        Title = "Wellington Let’s Roam Treasure Hunt:Cultural Capital!",
                         Date = DateTime.Now.AddMonths(7),
-                        Description = "Activity 7 months in future",
+                        Description = "See the best of Wellington and delight your friends with some epic scavenger hunt ideas for adults. On, Cultural Capital, you will wander with purpose to see the most entertaining sights in Wellington, and learn about the history and culture while you’re at it. Snap silly photos, answer tough trivia, and have a blast with the Let’s Roam scavenger hunt app!",
                         Category = "Travel",
-                        City = "Berlin",
-                        Venue = "All",
+                        City = "Wellington",
+                        Venue = "Frank Kitts Park",
                         UserActivities = new List<UserActivity>
                         {
                             new UserActivity
                             {
-                                AppUserId = "a",
+                                AppUserId = "g",
                                 IsHost = true,
                                 DateJoined = DateTime.Now.AddMonths(7)
                             },
                             new UserActivity
                             {
-                                AppUserId = "c",
+                                AppUserId = "a",
                                 IsHost = false,
                                 DateJoined = DateTime.Now.AddMonths(7)
                             },
@@ -255,12 +301,12 @@ namespace Persistence
                     },
                     new Activity
                     {
-                        Title = "Future Activity 8",
+                        Title = "Norma Jean - All Hail - Wellington",
                         Date = DateTime.Now.AddMonths(8),
-                        Description = "Activity 8 months in future",
+                        Description = "Buckle up, US metalcore mainstays Norma Jean are bringing the heaviness to New Zealand shores this May for their long awaited return off the back of their highly reviewed album 'All Hail'.",
                         Category = "Drinks",
-                        City = "London",
-                        Venue = "Pub",
+                        City = "Wellington",
+                        Venue = "Valhalla",
                         UserActivities = new List<UserActivity>
                         {
                             new UserActivity
@@ -271,7 +317,7 @@ namespace Persistence
                             },
                             new UserActivity
                             {
-                                AppUserId = "a",
+                                AppUserId = "d",
                                 IsHost = false,
                                 DateJoined = DateTime.Now.AddMonths(8)
                             },
