206a3cebc9c0ce1a41ce1c91a364ba983f6922b4
phuongha2012 <hami201289@him-mail.ch>
2020-09-05 12:24:52 +1200

    modify to fix deploy error logs

--- persistence/Seed.cs
+++ persistence/Seed.cs
@@ -82,7 +82,7 @@ namespace Persistence
                         Title = "Tribal Drum & Dance Jam",
                         Date = DateTime.Now.AddMonths(1),
                         Description = "Dancers are welcome to come along to enjoy dancing to live music and learn more about Middle Eastern rhythms - switch between drumming & dancing as suits you!",
-                        Category = "Music",
+                        Category = "music",
                         City = "Christchurch",
                         Venue = "Showbiz Studios",
                         UserActivities = new List<UserActivity>
@@ -118,7 +118,7 @@ namespace Persistence
                         Title = "Lunch on the lawn",
                         Date = DateTime.Now.AddMonths(2),
                         Description = "Come along to start the Term with a delicious free BBQ! Free snag or vege pattie as a welcome back to Term 4, see ya there!",
-                        Category = "Food",
+                        Category = "food",
                         City = "Auckland",
                         Venue = "Jamies Italian",
                         UserActivities = new List<UserActivity>
@@ -166,7 +166,7 @@ namespace Persistence
                         Title = "Gin Tasting at Riccarton House with Wine Divine",
                         Date = DateTime.Now.AddMonths(3),
                         Description = "The lovely Laura will take you through the history of some of the world’s best loved boutique gins along with some lesser known & unique bottles. Explore your taste buds by adding different garnishes & tonics to each gin. Paired nibbles will be served throughout the tasting.",
-                        Category = "Drinks",
+                        Category = "drinks",
                         City = "Canterbury",
                         Venue = "Riccarton House",
                         UserActivities = new List<UserActivity>
@@ -190,7 +190,7 @@ namespace Persistence
                         Title = "Banff Mountain Film Festival World Tour – WELLINGTON 2020",
                         Date = DateTime.Now.AddMonths(4),
                         Description = "Ignite your passion for adventure, action, and travel! The Banff Centre Mountain Film Festival World Tour will exhilarate you with amazing big-screen stories. Journey to exotic locations, paddle the wildest waters, and climb the highest peaks. Get your tickets today and be taken away to the most captivating places on earth.",
-                        Category = "Culture",
+                        Category = "culture",
                         City = "Wellington",
                         Venue = "Embassy Theatre",
                         UserActivities = new List<UserActivity>
@@ -220,7 +220,7 @@ namespace Persistence
                         Title = "High Tea at Le Cordon Bleu",
                         Date = DateTime.Now.AddMonths(5),
                         Description = "Experience an exquisite selection of the finest French Pâtisserie prepared by our highly creative and talented Le Cordon Bleu NZ Students",
-                        Category = "Drinks",
+                        Category = "drinks",
                         City = "Wellington",
                         Venue = "Le Cordon Bleu New Zealand",
                         UserActivities = new List<UserActivity>
@@ -256,7 +256,7 @@ namespace Persistence
                         Title = "Molly & the Chromatics live at Meow",
                         Date = DateTime.Now.AddMonths(6),
                         Description = "Molly & The Chromatics hit the road this month for two stand alone shows! The band have recently confirmed the release of their debut Album with the first track Goddess now out on all platforms. With impending releases on the very near horizon be sure to grab a ticket early to see this brand new live set from M&TC.",
-                        Category = "Music",
+                        Category = "music",
                         City = "Wellington",
                         Venue = "Meow",
                         UserActivities = new List<UserActivity>
@@ -280,7 +280,7 @@ namespace Persistence
                         Title = "Wellington Let’s Roam Treasure Hunt:Cultural Capital!",
                         Date = DateTime.Now.AddMonths(7),
                         Description = "See the best of Wellington and delight your friends with some epic scavenger hunt ideas for adults. On, Cultural Capital, you will wander with purpose to see the most entertaining sights in Wellington, and learn about the history and culture while you’re at it. Snap silly photos, answer tough trivia, and have a blast with the Let’s Roam scavenger hunt app!",
-                        Category = "Travel",
+                        Category = "travel",
                         City = "Wellington",
                         Venue = "Frank Kitts Park",
                         UserActivities = new List<UserActivity>
@@ -304,7 +304,7 @@ namespace Persistence
                         Title = "Norma Jean - All Hail - Wellington",
                         Date = DateTime.Now.AddMonths(8),
                         Description = "Buckle up, US metalcore mainstays Norma Jean are bringing the heaviness to New Zealand shores this May for their long awaited return off the back of their highly reviewed album 'All Hail'.",
-                        Category = "Drinks",
+                        Category = "drinks",
                         City = "Wellington",
                         Venue = "Valhalla",
                         UserActivities = new List<UserActivity>
